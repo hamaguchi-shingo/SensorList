60ca7176130304bf59cd00324a9a9f0aaf5b1302
hamaguchi-shingo <hamaguchi.shingoj@megachips.co.jp>
2016-06-27 22:29:34 +0900

    (refs #13) change layout

--- app/src/main/java/jp/co/pitta/sensorlist/SensorDisplayStepDetector.java
+++ app/src/main/java/jp/co/pitta/sensorlist/SensorDisplayStepDetector.java
@@ -3,6 +3,8 @@ package jp.co.pitta.sensorlist;
 import android.app.Activity;
 import android.hardware.SensorEvent;
 import android.hardware.TriggerEvent;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -11,18 +13,17 @@ import android.widget.TextView;
  * Created by shingo on 2016/06/02.
  */
 public class SensorDisplayStepDetector extends SensorDisplayBase {
-    Activity mActivity;
+
     ViewGroup mViewGroup;
     TableRow mTimestampTableRow;
     TableRow mSensorDataTableRow[];
 
     @Override
-    public void setUI(Activity activity) {
+    public void setUI(View view, LayoutInflater inflater) {
 
-        mActivity  = activity;
-        mViewGroup = (ViewGroup)mActivity.findViewById(R.id.tableLayout);
+        mViewGroup = (ViewGroup)view.findViewById(R.id.tableLayout);
 
-        mActivity.getLayoutInflater().inflate(R.layout.table_raw, mViewGroup);
+        inflater.inflate(R.layout.table_sensor_raw, mViewGroup);
         mTimestampTableRow = (TableRow)mViewGroup.getChildAt(0);
 
         ((TextView)mTimestampTableRow.getChildAt(0)).setText("timestamp");
@@ -34,7 +35,7 @@ public class SensorDisplayStepDetector extends SensorDisplayBase {
         mSensorDataTableRow = new TableRow[values.length];
 
         for(int i = 0; i < values.length; i++) {
-            mActivity.getLayoutInflater().inflate(R.layout.table_raw, mViewGroup);
+            inflater.inflate(R.layout.table_sensor_raw, mViewGroup);
             mSensorDataTableRow[i] = (TableRow) mViewGroup.getChildAt(i + 1);
             ((TextView) mSensorDataTableRow[i].getChildAt(0)).setText(values[i]);
             ((TextView) mSensorDataTableRow[i].getChildAt(2)).setText(unit[i]);
